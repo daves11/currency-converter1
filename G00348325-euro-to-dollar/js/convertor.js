b7f870565e193917ebc9aa2743c2940c26cdc2d3
daves11 <35769301+daves11@users.noreply.github.com>
2018-04-25 14:14:57 +0100

    Add files via upload

---
+++ G00348325-euro-to-dollar/js/convertor.js
@@ -0,0 +1,19 @@
+function euroToPounds(){
+
+  var amount = parseInt(document.getElementById("value1").value);
+
+  var pounds = amount * 0.86;
+  var message = amount + ' euros converts to ' + pounds + ' sterling.';
+  console.log(message);
+  document.getElementById("resultsentence").innerHTML = message;
+}
+//converts euros into US dollars
+function euroToUSDollar(){
+
+  var amount = parseInt(document.getElementById("value1").value);
+
+  var USDollar = amount * 1.06;
+  var message = amount + ' euros converts to ' + USDollar + 'USDollar.';
+  console.log(message);
+  document.getElementById("resultsentence").innerHTML = message;
+}
