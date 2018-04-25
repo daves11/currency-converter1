176c52f88381db1a1e717494bb66b256ef2777da
daves11 <35769301+daves11@users.noreply.github.com>
2018-04-25 14:20:18 +0100

    Add files via upload

---
+++ G00348325-euro-to-yen/js/convertor.js
@@ -0,0 +1,31 @@
+function euroToPounds(){
+
+  var amount = parseInt(document.getElementById("value1").value);
+
+  var pounds = amount * 0.86;
+  var message = amount + ' euros converts to ' + pounds + ' sterling.';
+  console.log(message);
+  document.getElementById("resultsentence").innerHTML = message;
+}
+
+//converts euros into US dollars
+function euroToUSDollar(){
+
+  var amount = parseInt(document.getElementById("value1").value);
+
+  var USDollar = amount * 1.06;
+  var message = amount + ' euros converts to ' + USDollar + 'USDollar.';
+  console.log(message);
+  document.getElementById("resultsentence").innerHTML = message;
+}
+
+//converts euros into Yen
+function euroToYen(){
+
+  var amount = parseInt(document.getElementById("value1").value);
+
+  var ToYen = amount * 121.12;
+  var message = amount + ' euros converts to ' + ToYen + 'Yen.';
+  console.log(message);
+  document.getElementById("resultsentence").innerHTML = message;
+}
